9f9ef3b6ff541a5b7bb6f1c964ac06e68dacad4e
PavelMorozov <pavevlmorozov75@gmail.com>
2024-05-10 21:53:25 +0300

    10_05_24

--- class_module_name.py
+++ class_module_name.py
@@ -15,3 +15,24 @@ print(a.__class__.__module__)
 print('C.__class__.__module__   ', C.__class__.__module__)
 #  не работает  print(a.__class__.__module__.__file__)
 #  не работает  print(a.__class__.__file__)
+
+
+
+from oop_examples import CC
+import sys, os
+class A:
+    pass
+print (sys.modules)
+print('sys.modules[A.__module__] : ', sys.modules[A.__module__])
+print('sys.modules[A.__module__].__name__ : ', sys.modules[A.__module__].__name__)
+print('sys.modules[A.__module__].__file__ : ', sys.modules[A.__module__].__file__)
+
+print('CC.__module__  ', CC.__module__)
+print('sys.modules[CC.__module__]   ', sys.modules[CC.__module__])
+print('sys.modules[CC.__module__].__file__   ', sys.modules[CC.__module__].__file__)
+
+
+print('__name__ ', __name__)
+print('__file__', __file__)
+print('sys.argv  ', sys.argv)
+print('os.getcwd()  ', os.getcwd())
--- oop_examples.py
+++ oop_examples.py
@@ -1,3 +1,5 @@
+class CC:
+    pass
 
 '''
 class Holyday:
